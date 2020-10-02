290d12a908ffa45185a17ec468a24f33d6e3954e
朔也 岩川 <31212@ojs.ac.jp>
2020-10-02 15:06:23 +0900

    第４章演習問題

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -11,26 +11,55 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-
-        }
-
-        private static string GetProduct()
-        {
-            Sale sale = new Sale
+            //4-2-1
+            var ymCollection = new YearMonth[]
             {
-                ShopName = "pet store",
-                Amount = 100000,
-                Product = "food"
+                new YearMonth (2000,1),
+                new YearMonth (2001,2),
+                new YearMonth (2002,3),
+                new YearMonth (2003,4),
+                new YearMonth (2004,5),
             };
-            sale = null;
-            return sale?.Product;
-        }
-    }
 
-    class Sale
-    {
-        public string ShopName { get; set; }
-        public int Amount { get; set; }
-        public string Product { get; set; }
+            //4-2-2
+            foreach(var ym in ymCollection)
+            {
+                Console.WriteLine(ym);
+            }
+        }
+
+        //4-2-3
+        static YearMonth FindFirst21C(YearMonth[] yms)
+        {
+            foreach(var ym in yms)
+            {
+                if (ym.Is21Century)
+                    return ym;
+            }
+            return null;
+        }
+
+        //4-2-4
+        private static void Exercise2_4(YearMonth[] ymCollection)
+        {
+            if(FindFirst21C(ymCollection) == null)
+            {
+                Console.WriteLine("21世紀のデータはありません");
+            }
+            else
+            {
+                Console.WriteLine(FindFirst21C(ymCollection));
+            }
+        }
+
+        //4-2-5
+        private static void Exercise2_5(YearMonth[] ymCollection)
+        {
+            var array = ymCollection.Select(ym => ym.AddOneMonth()).ToArray();
+            foreach(var ym in array)
+            {
+                Console.WriteLine(ym);
+            }
+        }
     }
 }
